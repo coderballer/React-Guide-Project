3b1a0149d4bda9db0b0e434ed8ffab86e6d33c06
coderballer <shhchen122@gmail.com>
2022-04-29 00:32:18 -0400

    add JSX and styling. closes #3

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import "./ExpenseItem.css"
+
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+    return (
+        <div className="expense-item">
+            <div>2022-08-29</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">USD 56.34</div>
+            </div>
+        </div >
+    );
 }
 
 export default ExpenseItem;
